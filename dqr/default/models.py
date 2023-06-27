5f77f161dddd41b9c03fbdbb784123573f37fe7a
numen <michaelbastauni@gmail.com>
2023-06-27 18:46:22 +0200

    implemented url redirection and modifyqrcode

--- dqr/default/models.py
+++ dqr/default/models.py
@@ -3,15 +3,10 @@ from django.db import models
 # Create your models here.
 class Qrcode(models.Model):
     name = models.CharField(max_length=200)
-    url = models.CharField(max_length=500)
-
-    def __str__(self):
-        return self.id +' - ' + self.name + ' - ' + self.url
-    
-class InternalUrl(models.Model):
     internal_url = models.CharField(max_length=500)
     external_url = models.CharField(max_length=500)
     modified = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='qrcodes/', blank=True, null=True)
 
     def __str__(self):
-        return self.id +' - ' + self.internal_url + ' - ' + self.external_url + ' - ' + self.modified
+        return self.id +' - ' + self.name + ' - ' + self.internal_url + ' - ' + self.external_url + ' - ' + self.modified
--- dqr/default/views.py
+++ dqr/default/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
 from default.models import *
+import hashlib
+import qrcode as qrcode_maker
 
 # Create your views here.
 def index(request):
@@ -10,10 +12,20 @@ def index(request):
 
 def addqrcode(request):
     if request.method == 'POST':
-        qrcode = Qrcode()
-        qrcode.name = request.POST['x-name']
-        qrcode.url = request.POST['x-url']
-        qrcode.save()
+        url = request.POST['x-url']
+
+        new_qrcode = Qrcode()
+        new_qrcode.name = request.POST['x-name']
+        new_qrcode.external_url = url
+        
+        # Hash the url
+        sha256_hash = hashlib.sha256()
+        sha256_hash.update(url.encode('utf-8'))
+        hashed_url = sha256_hash.hexdigest()
+        new_qrcode.internal_url = hashed_url # "http://michaelbasta.it/dqr/" + hashed_url
+
+        new_qrcode.save()
+
 
     return redirect(request.META.get('HTTP_REFERER', '/'))
 
@@ -22,5 +34,24 @@ def deleteqrcode(request):
         qrcode_id = request.POST['x-id']
         qrcode = Qrcode.objects.get(id=qrcode_id)
         qrcode.delete()
+
+    return redirect(request.META.get('HTTP_REFERER', '/'))
+
+def redirect_to_external_url(request, internal_url):
+    qrcode = Qrcode.objects.get(internal_url=internal_url)
+    return redirect(qrcode.external_url)
+
+def modifyqrcode(request):
+
+    if request.method == 'POST':
+        qrcode_id = request.POST['x-id']
+        qrcode = Qrcode.objects.get(id=qrcode_id)
+
+        if request.POST['x-name'] != '':
+            qrcode.name = request.POST['x-name']
+        if request.POST['x-url'] != '':
+            qrcode.external_url = request.POST['x-url']
+
+        qrcode.save()
 
     return redirect(request.META.get('HTTP_REFERER', '/'))
