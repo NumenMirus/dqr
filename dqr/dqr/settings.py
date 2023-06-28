0a541ea53e235a72a56824f1b0dba65980af898b
numen <michaelbastauni@gmail.com>
2023-06-28 17:14:05 +0200

    UnDockerized

--- dqr/dqr/settings.py
+++ dqr/dqr/settings.py
@@ -117,11 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = './static'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media'
+MEDIA_URL = os.path.join(BASE_DIR, 'appmedia/')
+MEDIA_ROOT = './appmedia'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
