cf80a0d76cea6fc6f488941314d702eca004a4b0
numen <michaelbastauni@gmail.com>
2023-06-29 20:23:02 +0200

    media fix

--- dqr/dqr/settings.py
+++ dqr/dqr/settings.py
@@ -117,10 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
 STATIC_ROOT = './static'
 
-MEDIA_URL = os.path.join(BASE_DIR, 'appmedia/')
+MEDIA_URL = './static/'
 MEDIA_ROOT = './appmedia'
 
 # Default primary key field type
