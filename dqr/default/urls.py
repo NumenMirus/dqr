b426aeb1bb90b26549a5eb74e67fedf435dd5aa7
numen <michaelbastauni@gmail.com>
2023-06-29 20:37:19 +0200

    changes

--- dqr/default/urls.py
+++ dqr/default/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('addqrcode', views.addqrcode, name='addqrcode'),
     path('deleteqrcode', views.deleteqrcode, name='deleteqrcode'),
-    path('redirect?<str:internal_url>', views.redirect_to_external_url, name='redirect_to_external_url'),
+    path('<str:internal_url>', views.redirect_to_external_url, name='redirect_to_external_url'),
     path('modifyqrcode', views.modifyqrcode, name='modifyqrcode'),
 ]
